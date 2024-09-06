bb581b76dbc0928c29daddefdfab29ec351f77e8
20sb <subhabiswal20@gmail.com>
2024-09-06 15:45:59 +0530

    Code updated for cpanel deploying with ssl setup

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
 const app = express();
 const port = 3000;
 
@@ -6,6 +9,18 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+try {
+    const httpsoptions = {
+        key: fs.readFileSync(`${process.env.SSL_PATH}/private.key`),
+        cert: fs.readFileSync(`${process.env.SSL_PATH}/certificate.crt`),
+        ca: fs.readFileSync(`${process.env.SSL_PATH}/ca.crt`),
+    };
+    https.createServer(httpsoptions, app).listen(port, () => {
+        console.log(`ATS App Server Listening at ${port}`);
+    });
+} catch (err) {
+    console.log("Error in Loding Certificates for ATS: ", err);
+    app.listen(port, () => {
+        console.log("ATS Production Server is running on port ", port);
+    });
+}
